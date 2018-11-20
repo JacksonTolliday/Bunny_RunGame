a8fca25363a5bb8c6d6cfc49a3a047c4620f3a98
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-11-20 12:09:59 -0500

    Updated from Brython Server: 11/20/2018 12:09:57 PM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -1,1 +1,19 @@
+class Rabbit(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/bunnysheet5.png",
+        Frame(25,217,35,32), 8, 'horizontal')
 
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        SpaceGame.listenKeyEvent("keydown", "a", self.rotl)
+        SpaceGame.listenKeyEvent("keydown", "d", self.rotr)
+        self.fxcenter = self.fycenter = 0.5
+        self.thrust = 0
+        self.thrustframe = 1
