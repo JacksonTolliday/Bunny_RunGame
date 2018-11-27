a9c63a9d022c052d27474b26ac524b8da2be50d0
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-11-27 12:55:34 -0500

    Updated from Brython Server: 11/27/2018 12:55:33 PM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -21,7 +21,7 @@ class Person(Sprite):
         self.fxcenter = self.fycenter = 0.5
         self.thrust = 0
         self.animater = 0
-        self.animater = 16
+        self.animatel = 16
 
 
     def step(self):
@@ -40,10 +40,10 @@ class Person(Sprite):
                 self.animater = 0
         if self.tv < 0:
             self.setImage(16)
-            self.setImage(self.animater)
-            self.animater -= 1
-            if self.animater == 8:
-                self.animater = 16
+            self.setImage(self.animatel)
+            self.animatel -= 1
+            if self.animatel == 8:
+                self.animatel = 16
         else:
             pass
         
