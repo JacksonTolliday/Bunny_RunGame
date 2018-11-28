86f510f387d0289539a080b82a3efa58a16faa9b
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-11-28 11:43:55 -0500

    Updated from Brython Server: 11/28/2018 11:43:54 AM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -18,7 +18,7 @@ class Person(Sprite):
         Frame(256, 64, 64, 64), 1, 'horizontal')
     asset.append("images/platformer_sprites_base.png",
         Frame(0, 512, 64, 64), 1, 'horizontal')
-    asset.append("images/platformer_sprites_base.png",
+    asset.append("images/platformer_sprites_base -left.png",
         Frame(512, 512, 64, 64), 1, 'horizontal')
     def __init__(self, position):
         super().__init__(Person.asset, position)
