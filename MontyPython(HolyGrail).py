6f7073b83c2ccf6e27703a6814c03bf30f8e98dc
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-12-03 11:58:08 -0500

    Updated from Brython Server: 12/3/2018 11:58:06 AM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -70,13 +70,13 @@ class Person(Sprite):
                 if self.tv > -3:
                     self.setImage(self.animatel)
                     print(self.animatel)
-                    self.animatel -= 0.05
+                    self.animatel -= 0.25
                     if self.animatel <= 8:
                         self.animatel = 14
             if self.tv < -2:
                 self.setImage(self.animatelr)
                 self.animatelr -= 0.25
-                if self.animatelr == 24:
+                if self.animatelr <= 24:
                     self.animatelr = 30
         else:
             pass
