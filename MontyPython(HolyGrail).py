f0856fbcc9fcd44e48ce9ea349245a9ac1bcc5cd
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-12-05 11:42:51 -0500

    Updated from Brython Server: 12/5/2018 11:42:50 AM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -104,9 +104,9 @@ class Game(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         pic_asset = ImageAsset("images/backgroundexperiment.png", Frame(0, 0, 960, 672), 1, 'horizontal')
-        pic_asset.scale = 2.0
         bg_asset = RectangleAsset(self.width, self.height, noline, black)
         bg = Sprite(pic_asset, (0,0))
+        bg.scale = 2.0
         Person((200,200))
 
 
