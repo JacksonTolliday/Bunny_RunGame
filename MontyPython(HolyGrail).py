2aa092205ac4d8529c44bcd6e002f65f3b4f0e51
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-12-07 12:10:21 -0500

    Updated from Brython Server: 12/7/2018 12:10:21 PM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -135,7 +135,7 @@ class Game(App):
                 self.bg2.x = -1152
             if self.bg2.x == 0:
                 self.bg1.x = -1152
-            if self.bg2.x == Game.width:
+            if self.bg2.x == (1152-Game.width):
                 self.bg1.x = 1152
             
 myapp = Game()
