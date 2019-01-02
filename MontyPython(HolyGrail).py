6cd7f77c1ab62e8b34eb95166f836101dc6ad9b4
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2019-01-02 07:52:17 -0500

    Updated from Brython Server: 1/2/2019 7:52:16 AM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -200,7 +200,6 @@ class Game(App):
         self.bg2.scale = 0.9
         self.p = Person((Game.width/2,400))
         self.os = ObstacleS((0,369))
-        self.oss = ObstacleS((Game.width,369))
 
     def step(self):
         for player in self.getSpritesbyClass(Person):
