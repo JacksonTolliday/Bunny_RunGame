8cc849ee9950a1d32e86df360e2230be4c095190
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2019-01-03 11:17:54 -0500

    Updated from Brython Server: 1/3/2019 11:17:53 AM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -163,12 +163,12 @@ class ObstacleS(Sprite):
         super().__init__(ObstacleS.asset, position)
         self.animateosr = 0
         self.animateosl = 8
-        global Game
-        self.left = Game.Pal.left
-        self.right = Game.Pal.right
         #ok, so 2 types of obstacles; 1 runs the other doesn't. simple enough.
 
     def step(self):
+        global Game
+        self.left = Game.Pal.left
+        self.right = Game.Pal.right
         self.x -= Game.Pal.tv - 4
         if self.left == True:
             self.x += Game.Pal.tv - 4 + Game.Pal.tv - 4
