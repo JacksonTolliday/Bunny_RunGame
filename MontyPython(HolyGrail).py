850359e2e23b64aec6ade9c793593ddf4eb590fe
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2019-01-09 22:31:02 -0500

    Updated from Brython Server: 1/9/2019 10:31:01 PM

--- MontyPython(HolyGrail).py
+++ MontyPython(HolyGrail).py
@@ -215,7 +215,7 @@ class Game(App):
         print(self.score)
         for player in self.getSpritesbyClass(Person):
             player.step()
-        if self.Pal.collidingWith(self.os))) == True
+        if self.Pal.collidingWith(self.os))) == True:
             self.x -= 1
             self.scorprint.destroy()
             self.scorprint = Score(self, (10,10))
