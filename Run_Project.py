ae9ca604ebe85866d11055c2c80f21a4f82837a9
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2019-01-11 10:33:14 -0500

    Updated from Brython Server: 1/11/2019 10:33:13 AM

--- Run_Project.py
+++ Run_Project.py
@@ -271,7 +271,6 @@ class Game(App):
                 self.os.destroy()
                 self.os = ObstacleS((-3*Game.width/4+1,369))
         else:
-            #print('Game Over!')
             self.gameprint = End(self, (Game.width/2-200,Game.height/2))
 
 Game().run()
